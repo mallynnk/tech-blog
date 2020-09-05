1775f23ef1949745e70651586823d468dbf29182
mallynnk <mallory.korpics@gmail.com>
2020-09-05 08:41:41 -0500

    added controllers

---
+++ controllers/api/comment-routes.js
@@ -0,0 +1,48 @@
+const router = require('express').Router();
+const withAuth = require('../../utils/auth');
+const { Comment } = require('../../models');
+
+router.get('/', (req, res) => {
+    Comment.findAll({
+        comment_text: req.body.comment_text,
+        user_id: req.body.user_id,
+        post_id: req.body.post_id
+    })
+    .then(dbCommentData => res.json(dbCommentData))
+    .catch(err => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+});
+
+router.post('/', (req, res) => {
+  // check the session
+  if (req.session) {
+    Comment.create({
+      comment_text: req.body.comment_text,
+      post_id: req.body.post_id,
+      // use the id from the session
+      user_id: req.session.user_id
+    })
+      .then(dbCommentData => res.json(dbCommentData))
+      .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  }
+});
+
+router.delete('/:id', withAuth, (req, res) => {
+    Comment.destroy({
+        comment_text: req.body.comment_text,
+        user_id: req.body.user_id,
+        post_id: req.body.post_id
+      })
+        .then(dbCommentData => res.json(dbCommentData))
+        .catch(err => {
+          console.log(err);
+          res.status(400).json(err);
+        });
+});
+
+module.exports = router;
