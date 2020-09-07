cf017338b01c2951a4b4bbccbd55ffad41401541
mallynnk <mallory.korpics@gmail.com>
2020-09-07 08:50:47 -0500

    added vote and fixed js files

--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -1,7 +1,33 @@
 async function editFormHandler(event) {
     event.preventDefault();
-  
-  }
-  
-  document.querySelector('.edit-post-form').addEventListener('submit', editFormHandler);
-  
+
+    const post_id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
+    ];
+
+    console.log("button clicked");
+
+    // const title = document.querySelector('input[name="post-title"]').value;
+    // const post_url = document.querySelector('textarea[name="post-content"]').value;
+
+    const title = document.querySelector('input[name="post-title"]').value.trim();
+    const post_url = document.querySelector('textarea[name="post-content"]').value.trim();
+    const response = await fetch(`/api/posts/${post_id}`, {
+        method: 'PUT',
+        body: JSON.stringify({
+            title,
+            post_url
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    if (response.ok) {
+        document.location.replace('/dashboard');
+    } else {
+        alert(response.statusText);
+    }
+}
+
+document.querySelector('.edit-post-form').addEventListener('submit', editFormHandler);
